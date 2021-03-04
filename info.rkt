1666bd442829c573d95e9ff4c3cc100bf942e586
Suzanne Soy <ligo@suzanne.soy>
2021-03-04 20:43:47 +0000

    Changed my name :)

--- info.rkt
+++ info.rkt
@@ -7,4 +7,4 @@
 (define scribblings '(("scribblings/backport-template-pr1514.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
-(define pkg-authors '(georges))
+(define pkg-authors '(|Suzanne Soy|))
